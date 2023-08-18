312f46c536384b5944100a53e6b625e08b268dc2
James Luty <luty0125@gmail.com>
2023-08-18 12:55:22 -0700

    css changes so sponsor form is mobile friendly

--- src/views/SponsorForm.jsx
+++ src/views/SponsorForm.jsx
@@ -19,6 +19,11 @@ const Sponsor_Form = () => {
     let validEmail = false;
     let navigate = useNavigate();
 
+    // EmailJs Information
+    const publicKey = "";
+    const templateId = "";
+    const serviceId = "";
+
     useEffect(() => {
         window.scrollTo(0,0);
     }, [])
@@ -80,7 +85,13 @@ const Sponsor_Form = () => {
         checkValues(firstName, lastName, phoneNumber, email);
 
         if(valid){
-            
+            emailjs.sendForm(serviceId, templateId, form.current, publicKey)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+            navigate("/");
         }
     }
 
@@ -90,7 +101,7 @@ const Sponsor_Form = () => {
             <p className="sponsorFormSubTitle">To become a sponsor, please fill out this form.</p>
             
             {/* Sponsor Form */}
-            <form onSubmit={sendEmail} className="sponsorForm">
+            <form onSubmit={sendEmail} ref={form} className="sponsorForm">
                 <div className="formLine">
                     <input type="text" name="company" id="company" placeholder='Company (optional)' className="companyInput" onChange={(e) => setCompany(e.target.value)} value={company}/>
                 </div>
@@ -120,60 +131,62 @@ const Sponsor_Form = () => {
             </form>
             
             {/* Platinum Sponsor Bundle */}
-            <div className="platinumBundle">
-                <h3 className="bundleTitle">Platinum - $5000.00</h3>
-                <ul>
-                    <li className="bundleText">EPRA Sponsor Buckle</li>
-                    <li className="bundleText">Bucking/Timed Event Chute Placard - One of seven (7) options</li>
-                    <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
-                    <li className="bundleText">Two Arena Banners - places inside the arena</li>
-                    <li className="bundleText">One Grandstand Banner - placed above the seating</li>
-                    <li className="bundleText">Rodeo Program Recognition / Advertising</li>
-                    <li className="bundleText">EPR Webpage Recognition</li>
-                    <li className="bundleText">Eight (8) General Admission Tickets - Each performance</li>
-                    <li className="bundleText">Eight (8) Catered VIP Hospitality Tickets - Each performance</li>
-                </ul>
-            </div>
+            <div className="bundles">
+                <div className="platinumBundle">
+                    <h3 className="bundleTitle">Platinum - $5000.00</h3>
+                    <ul>
+                        <li className="bundleText">EPRA Sponsor Buckle</li>
+                        <li className="bundleText">Bucking/Timed Event Chute Placard - One of seven (7) options</li>
+                        <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
+                        <li className="bundleText">Two Arena Banners - places inside the arena</li>
+                        <li className="bundleText">One Grandstand Banner - placed above the seating</li>
+                        <li className="bundleText">Rodeo Program Recognition / Advertising</li>
+                        <li className="bundleText">EPR Webpage Recognition</li>
+                        <li className="bundleText">Eight (8) General Admission Tickets - Each performance</li>
+                        <li className="bundleText">Eight (8) Catered VIP Hospitality Tickets - Each performance</li>
+                    </ul>
+                </div>
 
-            {/* Gold Sponsor Bundle */}
-            <div className="goldBundle">
-                <h3 className="bundleTitle">Gold - $3000.00</h3>
-                <ul>
-                    <li className="bundleText">Arena Gate Banner - One of four (4) options</li>
-                    <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
-                    <li className="bundleText">Two Arena Banners - places inside the arena</li>
-                    <li className="bundleText">One Grandstand Banner - placed above the seating</li>
-                    <li className="bundleText">Rodeo Program Recognition / Advertising</li>
-                    <li className="bundleText">EPR Webpage Recognition</li>
-                    <li className="bundleText">Six (6) General Admission Tickets - Each performance</li>
-                    <li className="bundleText">Six (6) Catered VIP Hospitality Tickets - Each performance</li>
-                </ul>
-            </div>
+                {/* Gold Sponsor Bundle */}
+                <div className="goldBundle">
+                    <h3 className="bundleTitle">Gold - $3000.00</h3>
+                    <ul>
+                        <li className="bundleText">Arena Gate Banner - One of four (4) options</li>
+                        <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
+                        <li className="bundleText">Two Arena Banners - places inside the arena</li>
+                        <li className="bundleText">One Grandstand Banner - placed above the seating</li>
+                        <li className="bundleText">Rodeo Program Recognition / Advertising</li>
+                        <li className="bundleText">EPR Webpage Recognition</li>
+                        <li className="bundleText">Six (6) General Admission Tickets - Each performance</li>
+                        <li className="bundleText">Six (6) Catered VIP Hospitality Tickets - Each performance</li>
+                    </ul>
+                </div>
 
-            {/* Silver Sponsor Bundle */}
-            <div className="silverBundle">
-                <h3 className="bundleTitle">Silver - $2000.00</h3>
-                <ul>
-                    <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
-                    <li className="bundleText">Two Arena Banners - places inside the arena</li>
-                    <li className="bundleText">One Grandstand Banner - placed above the seating</li>
-                    <li className="bundleText">Rodeo Program Recognition / Advertising</li>
-                    <li className="bundleText">EPR Webpage Recognition</li>
-                    <li className="bundleText">Six (6) General Admission Tickets - Each performance</li>
-                    <li className="bundleText">Six (6) Catered VIP Hospitality Tickets - Each performance</li>
-                </ul>
-            </div>
+                {/* Silver Sponsor Bundle */}
+                <div className="silverBundle">
+                    <h3 className="bundleTitle">Silver - $2000.00</h3>
+                    <ul>
+                        <li className="bundleText">Grand Entry / Event Flag - presented during each performance</li>
+                        <li className="bundleText">Two Arena Banners - places inside the arena</li>
+                        <li className="bundleText">One Grandstand Banner - placed above the seating</li>
+                        <li className="bundleText">Rodeo Program Recognition / Advertising</li>
+                        <li className="bundleText">EPR Webpage Recognition</li>
+                        <li className="bundleText">Six (6) General Admission Tickets - Each performance</li>
+                        <li className="bundleText">Six (6) Catered VIP Hospitality Tickets - Each performance</li>
+                    </ul>
+                </div>
 
-            {/* Bronze Sponsor Bundle */}
-            <div className="bronzeBundle">
-            <h3 className="bundleTitle">Bronze - $1000.00</h3>
-                <ul>
-                    <li className="bundleText">Two Grandstand Banner - placed above the seating</li>
-                    <li className="bundleText">Rodeo Program Recognition / Advertising</li>
-                    <li className="bundleText">EPR Webpage Recognition</li>
-                    <li className="bundleText">Four (4) General Admission Tickets - Each performance</li>
-                    <li className="bundleText">Four (4) Catered VIP Hospitality Tickets - Each performance</li>
-                </ul>
+                {/* Bronze Sponsor Bundle */}
+                <div className="bronzeBundle">
+                <h3 className="bundleTitle">Bronze - $1000.00</h3>
+                    <ul>
+                        <li className="bundleText">Two Grandstand Banner - placed above the seating</li>
+                        <li className="bundleText">Rodeo Program Recognition / Advertising</li>
+                        <li className="bundleText">EPR Webpage Recognition</li>
+                        <li className="bundleText">Four (4) General Admission Tickets - Each performance</li>
+                        <li className="bundleText">Four (4) Catered VIP Hospitality Tickets - Each performance</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
